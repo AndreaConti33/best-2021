dddbd3616c44d21f628b1395d63f5be78692b079
AndreaConti <andrea.conti23331@gmail.com>
2022-01-03 17:51:32 +0100

    Correzione

--- src/main/java/org/generation/italy/controller/IndexController.java
+++ src/main/java/org/generation/italy/controller/IndexController.java
@@ -1,13 +1,10 @@
 package org.generation.italy.controller;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,10 +15,6 @@ public class IndexController {
 	@GetMapping("/")
 
 	public String index(Model model, @RequestParam(name = "name", defaultValue = "Default") String nameParam) {
-
-		if (nameParam == null) {
-			nameParam = "Default";
-		}
 
 		model.addAttribute("name", "Andrea");
 
@@ -43,8 +36,8 @@ public class IndexController {
 
 	public String songs(Model model) {
 
-		model.addAttribute("songs", Arrays.asList("Kiss Me More","Zitti E Buoni","STAY","Friday"));
-		
+		model.addAttribute("songs", Arrays.asList("Kiss Me More", "Zitti E Buoni", "STAY", "Friday"));
+
 		return "songs";
 
 	}
